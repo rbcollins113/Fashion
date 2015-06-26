e890e39ccc515f6abc8f9c9779fa3cb098d595f4
Robert Collins <ptan35@gmail.com>
2015-06-27 02:36:59 +0700

    Asscociations and Scope

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ActiveRecord::Base
 	has_many :products
+	scope :sorted, ->{order(:id)}
+	scope :sort_by_name, ->{order(:id)}
 	scope :by_category, ->(cat){where(category: cat)}
 end
