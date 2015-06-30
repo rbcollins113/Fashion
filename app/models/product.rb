9166971ab06cf3cf902414551d1dfa73fce8640f
Robert Collins <ptan35@gmail.com>
2015-06-30 08:13:54 +0700

    add scope sort for products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,9 @@
 class Product < ActiveRecord::Base
 	belongs_to :category
-	scope :order_by_create_at,  -> { order("products.created_at ASC") }
+	scope :sort_by_created_at, -> {order("created_at DESC")}
+	scope :order_by_create_at,  -> { order("products.created_at DESC")}
+
 	def self.order_by_create_at
-		order("books.created_at ASC")
+		order("products.created_at DESC")
 	end
 end
