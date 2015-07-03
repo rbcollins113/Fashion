7a2811cc16a8467af9b4edf7ad45480a8311e323
Robert Collins <ptan35@gmail.com>
2015-07-03 09:06:31 +0700

    Upload Layouts for Website

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   resources :products
   root "products#index"
+  resources :categories, only: [:show, :index]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
