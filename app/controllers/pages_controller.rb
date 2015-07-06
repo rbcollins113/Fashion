ba02ae11e6a45d90c5d0e2bcfa49c632d7cec3ab
Robert Collins <ptan35@gmail.com>
2015-07-07 01:08:03 +0700

    Add methor index for page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
+  
+  def index
+  	@products = Product.sort_by_created_at.limit(12)
+  end
+
   def home
   	@products = Product.sort_by_created_at.limit(6)
   end
