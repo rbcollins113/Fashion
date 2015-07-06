4b7c7c584518c845cc3bd1e4c1126f9d32c8e08a
Robert Collins <ptan35@gmail.com>
2015-07-07 01:11:20 +0700

    set root for web

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get 'pages/contact'
 
   resources :products
-  root "products#index"
+  root "pages#index"
   resources :categories, only: [:show, :index]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
